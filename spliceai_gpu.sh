d05ed99e246eb19de759c542709b893007bf4dc1
Gavin Monahan <gavin.monahan@gmail.com>
2025-02-14 22:10:02 +0800

    gpu working

--- spliceai_gpu.sh
+++ spliceai_gpu.sh
@@ -17,10 +17,8 @@ module swap pawseyenv/2024.05 pawseyenv/2023.08
 module load gcc/12.2.0
 module load tensorflow/rocm5.6-tf2.12
 
-bash
-
 cd $MYSOFTWARE/manual/software/pythonEnvironments/tensorflowContainer-environments
 
 source spliceai2/bin/activate
 
-/home/gmonahan/.local/bin/spliceai -R /scratch/references/human/broad_hg19/Homo_sapiens_assembly19.fasta -A grch37 -I $MYSOFTWARE/T2T/SpliceAI/examples/input.vcf -O $MYSOFTWARE/T2T/SpliceAI/examples/test_out.vcf
+/software/setonix/2024.05/pawsey/software/shpc/lib/python3.11/site-packages/modules/quay.io/pawsey/tensorflow/2.12.1.570-rocm5.6.0/bin/python /home/gmonahan/.local/bin/spliceai -R /scratch/pawsey0933/gmonahan/hg19.fa.gz -A grch37 -I $MYSOFTWARE/T2T/SpliceAI/examples/input.vcf -O $MYSOFTWARE/T2T/SpliceAI/examples/test_out.vcf
