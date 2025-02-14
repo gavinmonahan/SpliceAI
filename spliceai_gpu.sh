2ac0fea310a71fa62332deb2de913732a16a7974
Gavin Monahan <gavin.monahan@gmail.com>
2025-02-14 23:23:10 +0800

    run for real

--- spliceai_gpu.sh
+++ spliceai_gpu.sh
@@ -21,4 +21,4 @@ cd $MYSOFTWARE/manual/software/pythonEnvironments/tensorflowContainer-environmen
 
 source spliceai2/bin/activate
 
-/software/setonix/2024.05/pawsey/software/shpc/lib/python3.11/site-packages/modules/quay.io/pawsey/tensorflow/2.12.1.570-rocm5.6.0/bin/python /home/gmonahan/.local/bin/spliceai -R /scratch/pawsey0933/gmonahan/hg19.fa.gz -A grch37 -I $MYSOFTWARE/T2T/SpliceAI/examples/input.vcf -O $MYSOFTWARE/T2T/SpliceAI/examples/test_out.vcf
+/software/setonix/2024.05/pawsey/software/shpc/lib/python3.11/site-packages/modules/quay.io/pawsey/tensorflow/2.12.1.570-rocm5.6.0/bin/python /home/gmonahan/.local/bin/spliceai -R /scratch/pawsey0933/T2T/reference/chm13v2.0.maskedY.rCRS.EBV.fasta -A $MYSOFTWARE/T2T/SpliceAI/spliceai/annotations/chm13_all.txt -I /scratch/pawsey0933/T2T/spliceai/T2T_snps_dysgu_VEP_genmod_annotated_qc_af_GeneticModels.vcf.gz -O /scratch/pawsey0933/T2T/spliceai/T2T_snps_dysgu_VEP_genmod_annotated_qc_af_GeneticModels_spliceai.vcf
